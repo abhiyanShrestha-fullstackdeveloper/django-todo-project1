a28c581381f93a5c0a14dce140e356702ffd5920
abhiyanShrestha-fullstackdeveloper <abhiyanshrestha328@gmail.com>
2024-08-22 07:57:05 +0545

    new update
    created an super admin for the django project and imported todo to the admin dashboard and for requesting the backend data in front end of the website added todo_objs to call todo model and for sending file to front end of the table data variable is created for it and made sure the program is running in loop for the table using {%%} in it and connected the databse with the table front end as the code is shown and created create.html for user to create an todo with all the url connected with it and connections so it runs in the server of the program

--- todo/base/views.py
+++ todo/base/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html')
+    todo_objs = Todo.objects.all() # Runs all the query from the todo table
+    data = {'todos': todo_objs} # Send file from data variable as support 
+    return render(request,'index.html',context=data)
+
+def create(request):
+    return render(request,'create.html')
--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from base.views import home
+from base.views import create
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home)
+    path('',home),
+    path('create/',create),
 ]
